d1bf526469270b4c58a1efa482f7fcdd8d970526
amarmohite2001 <amarmohite200@gmail.com>
2024-02-18 21:55:39 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Hello Amar Mohite!"));
+app.get("/", (req, res) =>
+  res.send("Hello Amar Mohite!, Ready for deployment")
+);
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
